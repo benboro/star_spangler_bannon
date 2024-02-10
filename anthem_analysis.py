5c3ecb4c1c0c31d70bbc9ea2af75c44b1b205e50
Ben Borovinsky <bmborovinsky@gmail.com>
2024-02-10 13:50:17 -0800

    Run bref at 63

--- anthem_analysis.py
+++ anthem_analysis.py
@@ -11,7 +11,7 @@ score_data = pd.read_csv(os.path.join(directory, 'score_anthem_data.csv'))
 
 # 'Time' is a string data type, need to convert to 'Seconds' (integer data type)
 score_data['Seconds'] = score_data.apply(lambda x: aut.minutes_to_seconds(x['Time']), axis=1)
-target_time = 89.5
+target_time = 63
 
 # run analysis on Star Spangled Banner lyrics
-notes_data = aut.run_lyrics_analysis(song_duration=target_time, directory=directory, bref=False)
+notes_data = aut.run_lyrics_analysis(song_duration=target_time, directory=directory, bref=True)
